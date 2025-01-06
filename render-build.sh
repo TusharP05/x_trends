21a988ee2ff8e5af6d49777d216899b19652e3e2
Tushar Parakh <21BCS4197@CUCHD.IN>
2025-01-06 19:26:29 +0530

    render config

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# exit on error
 set -o errexit
 
 STORAGE_DIR=/opt/render/project/.render
@@ -8,17 +9,9 @@ if [[ ! -d $STORAGE_DIR/chrome ]]; then
    echo "...Downloading Chrome"
    mkdir -p $STORAGE_DIR/chrome
    cd $STORAGE_DIR/chrome
-   
-   # Download Chrome .deb package
    wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-   
-   # Extract Chrome package
    dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
-   
-   # Remove the downloaded .deb file
    rm ./google-chrome-stable_current_amd64.deb
-   
-   # Return to original directory
    cd $HOME/project
 else
    echo "...Using Chrome from cache"
@@ -30,22 +23,18 @@ CHROMEDRIVER_URL=$(curl -sS "https://chromedriver.storage.googleapis.com/LATEST_
 CHROMEDRIVER_DOWNLOAD="https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_URL/chromedriver_linux64.zip"
 
 echo "Downloading ChromeDriver version: $CHROMEDRIVER_URL"
-mkdir -p /opt/render/project/src/.render/
-wget -O "/opt/render/project/src/.render/chromedriver.zip" "$CHROMEDRIVER_DOWNLOAD"
+wget -O "chromedriver.zip" "$CHROMEDRIVER_DOWNLOAD"
+unzip -o "chromedriver.zip"
+chmod +x chromedriver
+mv chromedriver /usr/local/bin/
 
-# Extract ChromeDriver
-unzip -o "/opt/render/project/src/.render/chromedriver.zip" -d /opt/render/project/src/.render/
-chmod +x /opt/render/project/src/.render/chromedriver
-
-# Print versions for verification
+# Print versions
 echo "Chrome version:"
 $STORAGE_DIR/chrome/opt/google/chrome/google-chrome --version
-
 echo "ChromeDriver version:"
-/opt/render/project/src/.render/chromedriver --version
+chromedriver --version
 
 # Install Python dependencies
-pip install --upgrade pip
 pip install -r requirements.txt
 
-echo "Build process completed successfully"
+echo "Build completed successfully"
